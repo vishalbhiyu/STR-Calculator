8e923977bfb951db28f2d60eff5c90ccd0f426e5
Vishalbhiyu <vishalbhaiyu171@gmail.com>
2025-04-01 09:41:06 +0530

    Add multi-character delimiter support

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -46,4 +46,9 @@ RSpec.describe StringCalculator do
             expect(StringCalculator.add('2,1001')).to eq(2)
             expect(StringCalculator.add('1000,1001')).to eq(1000)
           end
+
+          it 'handles multi-character delimiters' do
+            expect(StringCalculator.add("//[***]\n1***2***3")).to eq(6)
+            expect(StringCalculator.add("//[---]\n1---2---3")).to eq(6)
+          end
 end
