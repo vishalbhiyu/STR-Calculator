4cd836d12989fab60e575212d4856c62be24996b
Vishalbhiyu <vishalbhaiyu171@gmail.com>
2025-04-01 09:40:24 +0530

    Filter out numbers greater than 1000

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -41,4 +41,9 @@ RSpec.describe StringCalculator do
             expect { StringCalculator.add('1,-2') }.to raise_error('negatives not allowed: -2')
             expect { StringCalculator.add('-1,-2,3') }.to raise_error('negatives not allowed: -1, -2')
           end
+
+          it 'ignores numbers larger than 1000' do
+            expect(StringCalculator.add('2,1001')).to eq(2)
+            expect(StringCalculator.add('1000,1001')).to eq(1000)
+          end
 end
